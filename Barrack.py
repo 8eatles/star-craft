cdc6745257d98605f81b99ee7506c356480f5988
jongchan.kim <8eatles@naver.com>
2022-11-12 12:47:28 +0900

    Python project Practice

---
+++ Barrack.py
@@ -0,0 +1,16 @@
+from Firebat import Firebat
+
+
+class Barrack:
+    def __init__(self, player):
+        self.player = player
+
+    def produce(self):
+        self.player.mineral -= 50
+        self.player.gas -= 25
+        f = open("log.csv", "a")
+        f.write(f"{self.player.name},파이어뱃,생성,1\n")
+        f.close()
+        return Firebat(self.player)
+
+
---
+++ Player.py
@@ -0,0 +1,8 @@
+class Player:
+    # 생성자
+    def __init__(self, name):
+        self.name = name
+        self.mineral = 50
+
+    def __str__(self):
+        return f"{self.name}님의 보유 미네랄 양은 {self.mineral} 입니다."
